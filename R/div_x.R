f5d87c8eb5b1dd19bbe739388fb7cf26940261bd
VEZY Rémi <VEZY@users.noreply.github.com>
2019-02-02 10:33:08 +0100

    Add div_x function + update docs and website

---
+++ R/div_x.R
@@ -0,0 +1,15 @@
+#' Divide
+#'
+#' @description Divide x by y
+#'
+#' @param x The numerator
+#' @param y The denominator
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' div_x(2,5)
+div_x= function(x,y){
+  x/y
+}
