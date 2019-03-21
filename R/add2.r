41c4321ac8e68b63d6abc49add3ffdc8da0806ae
GinerM <giner@cirad.fr>
2019-03-21 17:09:28 +0100

    Function Add2
    Rajout de la fonction add2

---
+++ R/add2.r
@@ -0,0 +1,15 @@
+#' Add two
+#'
+#' @description Adds 2 to the input
+#'
+#' @param x Double or Integer
+#'
+#' @return x+2
+#' @export
+#'
+#' @examples
+#' add_x(2)
+#'
+add_x= function(x){
+  x+2
+}
